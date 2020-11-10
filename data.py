e47730f8036fa9749f9a9b36c4b9b165dc88c23f
Macelux <macelux@hotmail.com>
2020-11-10 10:47:31 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Mac',
+            'description': 'another desc',
+            'author': 'Mac Chika',
+            'create_date': '09-12-2002'
+        },
+         {
+            'id': 2,
+            'title': 'Brad',
+            'description': 'another name',
+            'author': 'Mac Chika',
+            'create_date': '09-12-2002'
+        }
+    ]
+    return articles
